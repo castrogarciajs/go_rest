9dd091cea6c01363e5d127c8d15b36bf3c498881
Sebastian009w <ia3266595@gmail.com>
2023-05-16 17:14:37 -0500

    server en go

--- main.go
+++ main.go
@@ -2,29 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
-	app := fiber.New()
-
-	app.Use(cors.New())
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(&fiber.Map{
-			"Root":  "Ruta inicial",
-			"Users": "http://localhost:3000",
-		})
-	})
-
-	app.Get("/users", func(c *fiber.Ctx) error {
-		return c.JSON(&fiber.Map{
-			"data": "usuarios desde el backend",
-		})
-	})
-
-	app.Listen(":3000")
 	fmt.Println("Server succesfuly in port 3000")
 }

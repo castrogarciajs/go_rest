071a78f4264418e8219ec96ae23376b094277671
Sebastian009w <ia3266595@gmail.com>
2023-05-16 16:57:34 -0500

    instalacion de cors y comunicacion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,16 @@
 function App() {
-  return <div>App</div>;
+  const GET = async () => {
+    const res = await fetch("http://localhost:3000/users");
+
+    const data = res.json();
+
+    console.log(data);
+  };
+  return (
+    <div>
+      <button onClick={GET}>JSON</button>
+    </div>
+  );
 }
 
 export default App;

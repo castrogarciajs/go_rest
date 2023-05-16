dc23fda108726358f67b28cc56a86e7c693d43ee
Sebastian009w <ia3266595@gmail.com>
2023-05-16 16:47:56 -0500

    configurando cliente

--- main.go
+++ main.go
@@ -9,6 +9,12 @@ import (
 func main() {
 	app := fiber.New()
 
+	app.Get("/users", func(c *fiber.Ctx) error {
+		return c.JSON(&fiber.Map{
+			"data": "usuarios desde el backend",
+		})
+	})
+
 	app.Listen(":3000")
 	fmt.Println("Server succesfuly in port 3000")
 }

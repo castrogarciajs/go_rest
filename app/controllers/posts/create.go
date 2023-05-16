c8b164a859da31015b82219b4e978b209066f8fd
Sebastian009w <ia3266595@gmail.com>
2023-05-16 17:46:54 -0500

    Funcion create

--- app/controllers/posts/create.go
+++ app/controllers/posts/create.go
@@ -1,1 +1,21 @@
 package posts
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/sebastian009w/go_rest/database"
+	"github.com/sebastian009w/go_rest/models"
+)
+
+func Create(ctx *fiber.Ctx) error {
+	post := models.Post{}
+
+	if err := ctx.BodyParser(&post); err != nil {
+		return ctx.Status(503).JSON(err)
+	}
+
+	if err := database.Conn.Create(&post).Error; err != nil {
+		return ctx.Status(503).JSON(err)
+	}
+
+	return ctx.JSON(post)
+}
